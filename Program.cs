10ae81361d618400f6ba857afeb25c97eac2922f
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-05 13:48:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using CADelegate;
+using System.ComponentModel.Design;
+
+internal class Program
+{
+     static void Main(string[] args)
+    {
+        var emps = new Employee[]
+        {
+            new Employee {Id = 1 , Name = "Issam A",Gender="M",TotalSales=65000m},
+            new Employee {Id = 2 , Name = "Reem S",Gender="F",TotalSales=50000m},
+            new Employee {Id = 3 , Name = "Suzan B",Gender="F",TotalSales=65000m},
+            new Employee {Id = 4 , Name = "Sara A",Gender="F",TotalSales=400000m},
+            new Employee {Id = 5 , Name = "Salah C ",Gender="M",TotalSales=42000m},
+            new Employee {Id = 6 , Name = "Rateb A",Gender="M",TotalSales=30000m},
+            new Employee {Id = 7 , Name = "Abeer C ",Gender="F",TotalSales=16000m},
+            new Employee {Id = 8 , Name = "Marwan M",Gender="M",TotalSales=15000m}
+        };
+        var report = new Report();
+
+        report.ProcessEmployeeWith60000PlusSales(emps);
+        report.ProcessEmployeeWithSalesBetween300000And59999(emps);
+        report.ProcessEmployeeWithSalesLessThan300000And59999(emps);
+        }
+    }
---
+++ Report.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADelegate
+{
+    internal class Report
+    {
+        public void ProcessEmployeeWith60000PlusSales(Employee[] employees)
+        {
+            Console.WriteLine("Employees With $60,000+Sales");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            foreach (var e in employees) {
+                if (e.TotalSales >= 60000m)
+                {
+                    Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | ${e.TotalSales}");
+                }
+            }
+            Console.WriteLine("\n\n");
+
+        }
+        public void ProcessEmployeeWithSalesBetween300000And59999(Employee[] employees)
+        {
+            Console.WriteLine("Employees With sales between $30,0000 And $59,999 Sales");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            foreach (var e in employees)
+            {
+                if (e.TotalSales < 60000m && e.TotalSales >= 300000m)
+                {
+                    Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | ${e.TotalSales}");
+                }
+            }
+            Console.WriteLine("\n\n");
+
+        }
+        public void ProcessEmployeeWithSalesLessThan300000And59999(Employee[] employees)
+        {
+            Console.WriteLine("Employees Less than sales between $30,0000 And $59,999 Sales");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            foreach (var e in employees)
+            {
+                if (e.TotalSales < 30000m )
+                {
+                    Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | ${e.TotalSales}");
+                }
+            }
+            Console.WriteLine("\n\n");
+
+        }
+    } }
